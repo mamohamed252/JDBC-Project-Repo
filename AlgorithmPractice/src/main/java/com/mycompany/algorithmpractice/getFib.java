163827ea08ee2b8a1341d485ba2cfa0444690f2c
mamohamed252 <ma.mohamed252@gmail.com>
2020-11-13 20:17:16 -0600

    Fib sequence

--- AlgorithmPractice/src/main/java/com/mycompany/algorithmpractice/getFib.java
+++ AlgorithmPractice/src/main/java/com/mycompany/algorithmpractice/getFib.java
@@ -10,8 +10,37 @@ package com.mycompany.algorithmpractice;
  * @author Mohamed
  */
 public class getFib {
+
     public static void main(String[] args) {
-        int a = 6;
+        int lengthOfPointer = 2;
+        System.out.println(fibMath(lengthOfPointer));
     }
-    
+
+    public static int fibMath(int userInput) {
+
+        int startingPoint = 0;
+        int nextPosition = 1;
+        int sumOfSN = startingPoint + nextPosition;
+
+        if (userInput == 0) {
+            return 0;
+        } else if (userInput == 1) {
+            return 1;
+        }
+
+//        while (userInput > 1) {
+//            sumOfSN = startingPoint + nextPosition;
+//            counter = sumOfSN;
+//            userInput;
+//        }
+        for (int i = 0; i < userInput-2; i++) {
+            if (userInput > 1) {
+                sumOfSN = startingPoint + nextPosition;
+                startingPoint = nextPosition;
+                nextPosition = sumOfSN;
+            }
+
+        }
+        return sumOfSN;
+    }
 }
